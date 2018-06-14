f9cd9a951c7b5e7671a29dadcb1955086bb879b7
Julia <hindsjr@appstate.edu>
2018-06-14 11:36:47 -0500

    added possibly solutions

--- panstarrs_test.py
+++ panstarrs_test.py
@@ -1,8 +1,9 @@
 
 #importing things needed
 import numpy as np
-import urlparse
+from urllib.parse import urlparse
 import os.path
+from posixpath import basename, dirname
 
 #I will use this later
 #for i in range(0,M-1):
@@ -17,25 +18,32 @@ import os.path
 #    print("Projection Cell: ", prodcell)
 #    print("Subcell: ", subcell)
 
-for p,c in range(635,2008), range(1,100):
+for p,s in range(635,2008), range(1,100):
     repos = np.DataSource()
-    repos.exists('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.c)
+    repos.exists('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s)
                  True
 
 #not sure if this reads the file or just downloads to /Documents/panstarrs/data
 #    ds = DataSource('/Documents/panstarrs/data')
-#    urlname = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.c'
-#    gfile = ds.open('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.c')  # remote file
+#    urlname = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s'
+#    gfile = ds.open('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s')  # remote file
 #    ds.abspath(urlname)
 
-    path = urlparse.urlparse('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.c').path   
-    print path
+    path = urlparse.urlparse('http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s').path   
+        print path
+
+def urlfunc(): #this will need to go in the loop
+    url = 'http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s'
+    parse_object = urlparse(url)
+    a = dirname(parse_object.path) #so will this return ps1filenames.py?skycell=p.s? Because we just want p and s
+    return a 
+
 
     os.path.split(path)
 
 
 #not sure if these will work
-#    url = np.DataSource.open(destpath="http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.c")
+#    url = np.DataSource.open(destpath="http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=p.s")
 
 #def analyze(filename):
     #data = np.loadtxt(http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?skycell=1405.053,
